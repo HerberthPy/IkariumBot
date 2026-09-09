b60d6e626db3b301acf767dc8836bcc9620504eb
HerberthPy <herberthdesigner@outlook.com>
2026-09-09 16:15:48 -0300

    Frontend em andamento / LOGIN, CADASTRO e DASH

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     #Chave de segurança para assinar sessões e cookies
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fuzileirosnavais'
 
     # Caminho do Banco de Dados SQLite (Salvo na pasta do projeto)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
